6017fd20940f814b488bd35d5af6488d96f98c11
Abhishek Soni <41066851+abhisheksoni0073@users.noreply.github.com>
2020-04-30 20:39:34 +0530

    Add files via upload

---
+++ php-1/page.php
@@ -0,0 +1,21 @@
+<?php
+// variable 
+#echo $_POST['n'] + $_POST['n1'];
+
+/*$num = $_POST['n'];
+$num1 = $_POST['n1'];
+$sum = $num+$num1;
+echo "<h1> Sun of $num + $num1 = ".$sum. "</h1>";*/
+// variable 
+// interger , string , boolean , float 
+$num = 1000;
+var_dump($num);
+echo "<br>";
+$string = "AWC college ";
+var_dump($string);
+echo "<br>";
+$bool = true;
+var_dump($bool);
+$f = 50.50;
+var_dump($f);
+?>
